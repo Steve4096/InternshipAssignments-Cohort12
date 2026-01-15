f907f7bd621ab0ffbef79d24dbf2cafbfc47d7e9
steve 4096 <smuturi795@gmail.com>
2026-01-15 09:17:29 +0300

    Initial commit

---
+++ src/main/java/org/example/PersonalIntroduction.java
@@ -0,0 +1,11 @@
+public class PersonalIntroduction {
+
+    public static void introduceMe(){
+        String name="Tevin Steve Muturi";
+        String goals="Become so proficient in Java that I can solve any problem programmatically with it.";
+        String favouriteConcept="OOP";
+        System.out.println("Name: "+name);
+        System.out.println("Goals: "+goals);
+        System.out.println("Excited to learn: "+ favouriteConcept);
+    }
+}
